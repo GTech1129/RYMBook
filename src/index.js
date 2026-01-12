2e03a8184914211da9dde7fdb99bffeb9c0ccbfa
GTech1129 <gtechdynamics@gmail.com>
2026-01-12 00:10:44 -0500

    Initialize React application with BookLaunchHero component

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BookLaunchHero from './bookxxx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookLaunchHero />);
